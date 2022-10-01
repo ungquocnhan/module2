86c23cdb9c728a337d1f51c037991af26dc56ff6
ungquocnhan <nhan09bk@gmail.com>
2022-10-01 08:13:15 +0700

    module2

--- src/ss16_binary_file/exercise/copy_file_binary_file/CopyFile.java
+++ src/ss16_binary_file/exercise/copy_file_binary_file/CopyFile.java
@@ -10,18 +10,12 @@ public class CopyFile {
     public static final String TARGET_CSV = "F:\\CODEGYM\\module_2\\src\\ss16_binary_file\\exercise\\copy_file_binary_file\\target.csv";
 
 
-    private static void writeFile() {
+    private static void writeFile(String string) {
         FileOutputStream fos;
         ObjectOutputStream oos = null;
         try {
             fos = new FileOutputStream(SOURCE_CSV);
             oos = new ObjectOutputStream(fos);
-
-            String string = "Viết một chương trình cho phép người dùng sao chép các tập tin. Người sử dụng cần phải cung cấp một tập tin nguồn (source file) và một tập tin đích (target file).\n" +
-                    "\n" +
-                    "Chương trình sao chép tệp nguồn vào tệp tin đích và hiển thị số byte trong tệp. Chương trình nên cảnh báo người dùng nếu tệp nguồn không tồn tại hoặc nếu tập tin đích đã tồn tại.\n" +
-                    "\n" +
-                    "Để sao chép nội dung từ tệp tin nguồn đến tệp tin đích, bạn nên sử dụng luồng đầu vào để đọc byte từ tệp nguồn và luồng đầu ra để gửi byte đến tệp tin đích, bất kể nội dung của tệp.";
             oos.writeObject(string);
         } catch (IOException e) {
             e.printStackTrace();
@@ -77,7 +71,12 @@ public class CopyFile {
     }
 
     public static void main(String[] args) {
-        CopyFile.writeFile();
+        String string = "Viết một chương trình cho phép người dùng sao chép các tập tin. Người sử dụng cần phải cung cấp một tập tin nguồn (source file) và một tập tin đích (target file).\n" +
+                "\n" +
+                "Chương trình sao chép tệp nguồn vào tệp tin đích và hiển thị số byte trong tệp. Chương trình nên cảnh báo người dùng nếu tệp nguồn không tồn tại hoặc nếu tập tin đích đã tồn tại.\n" +
+                "\n" +
+                "Để sao chép nội dung từ tệp tin nguồn đến tệp tin đích, bạn nên sử dụng luồng đầu vào để đọc byte từ tệp nguồn và luồng đầu ra để gửi byte đến tệp tin đích, bất kể nội dung của tệp.";
+        CopyFile.writeFile(string);
         File sourceFile = new File(SOURCE_CSV);
         File targetFile = new File(TARGET_CSV);
         readAndWriteFile(sourceFile, targetFile);
